9eb6bd015be73a27c69a55acb7bec53787918087
Oleksandr Kachanov <54705048+Zonathrope@users.noreply.github.com>
2022-05-09 21:07:41 +0300

    initialize bot

---
+++ index.js
@@ -0,0 +1,13 @@
+import './env.js';
+import { Telegraf } from 'telegraf'
+
+const bot = new Telegraf(process.env.TELEGRAM_KEY)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
