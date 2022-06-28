4344e25c75cf6825964b847783504872987ab3b0
Oleksandr Kachanov <54705048+Zonathrope@users.noreply.github.com>
2022-06-28 20:06:36 +0300

    saa

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
-console.log('aaaa')
+console.log('aaaaaa')
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
